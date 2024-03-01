ae8d7b4105de5a946876ed40f85699ea627b47a8
||We(r)K3|R|| <122714529+Pixelgameweb@users.noreply.github.com>
2024-03-01 19:03:09 +0200

    Add files

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// 1 задание
+// const cityList = document.querySelectorAll('ul li');
+// let cities = [];
+
+// cityList.forEach(city => {
+//   cities.push(city.textContent);
+// });
+
+// const citiesJSON = JSON.stringify({ cities });
+// console.log(citiesJSON);
+//----------------------------------------------------------------------------------------------------//
+// 2 задание
+
+
+// let json = '["user1","user2","user3","user4","user5"]';
+// let parsedJson = JSON.parse(json);
+
+// parsedJson.push("user6");
+
+// console.log(parsedJson);
+//----------------------------------------------------------------------------------------------------//
+// 3 задание
+
+
+// let json = `[
+// 	{
+// 		"name": "user1",
+// 		"age": 25,
+// 		"salary": 1000
+// 	},
+// 	{
+// 		"name": "user2",
+// 		"age": 26,
+// 		"salary": 2000
+// 	},
+// 	{
+// 		"name": "user3",
+// 		"age": 27,
+// 		"salary": 3000
+// 	}
+//     {
+// 		"name": "user4",
+// 		"age": 28,
+// 		"salary": 4000
+// 	}
+// ]`;
+
+
+//----------------------------------------------------------------------------------------------------//
+// 4 задание
+
+// let json = '["user1","user2","user3","user4","user5"]';
+// let users = JSON.parse(json);
+
+// document.write('<ul>');
+// users.forEach(user => {
+//     document.write(`<li>${user}</li>`);
+// });
+//Вывел на документ :)
+
+
+
+
+
