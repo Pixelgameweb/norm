e12e68778f7bd80025bf6f25a7f94d3e92993718
||We(r)K3|R|| <122714529+Pixelgameweb@users.noreply.github.com>
2024-03-02 09:03:10 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,61 +1,61 @@
 // 1 задание
-// const cityList = document.querySelectorAll('ul li');
-// let cities = [];
+const cityList = document.querySelectorAll('ul li');
+let cities = [];
 
-// cityList.forEach(city => {
-//   cities.push(city.textContent);
-// });
+cityList.forEach(city => {
+  cities.push(city.textContent);
+});
 
-// const citiesJSON = JSON.stringify({ cities });
-// console.log(citiesJSON);
+const citiesJSON = JSON.stringify({ cities });
+console.log(citiesJSON);
 //----------------------------------------------------------------------------------------------------//
 // 2 задание
 
 
-// let json = '["user1","user2","user3","user4","user5"]';
-// let parsedJson = JSON.parse(json);
+let json = '["user1","user2","user3","user4","user5"]';
+let parsedJson = JSON.parse(json);
 
-// parsedJson.push("user6");
+parsedJson.push("user6");
 
-// console.log(parsedJson);
+console.log(parsedJson);
 //----------------------------------------------------------------------------------------------------//
 // 3 задание
 
 
-// let json = `[
-// 	{
-// 		"name": "user1",
-// 		"age": 25,
-// 		"salary": 1000
-// 	},
-// 	{
-// 		"name": "user2",
-// 		"age": 26,
-// 		"salary": 2000
-// 	},
-// 	{
-// 		"name": "user3",
-// 		"age": 27,
-// 		"salary": 3000
-// 	}
-//     {
-// 		"name": "user4",
-// 		"age": 28,
-// 		"salary": 4000
-// 	}
-// ]`;
+let json = `[
+	{
+		"name": "user1",
+		"age": 25,
+		"salary": 1000
+	},
+	{
+		"name": "user2",
+		"age": 26,
+		"salary": 2000
+	},
+	{
+		"name": "user3",
+		"age": 27,
+		"salary": 3000
+	}
+    {
+		"name": "user4",
+		"age": 28,
+		"salary": 4000
+	}
+]`;
 
 
 //----------------------------------------------------------------------------------------------------//
 // 4 задание
 
-// let json = '["user1","user2","user3","user4","user5"]';
-// let users = JSON.parse(json);
+let json = '["user1","user2","user3","user4","user5"]';
+let users = JSON.parse(json);
 
-// document.write('<ul>');
-// users.forEach(user => {
-//     document.write(`<li>${user}</li>`);
-// });
+document.write('<ul>');
+users.forEach(user => {
+    document.write(`<li>${user}</li>`);
+});
 //Вывел на документ :)
 
 
